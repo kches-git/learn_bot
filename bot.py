6a0134ad4d5146b35deba09c8fa0532504f6fdc1
kches <k.chessnokov@yandex.ru>
2020-05-24 17:40:11 +0300

    Add planet() function to bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,10 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
+import ephem
 
-logging.basicConfig(filename="bot.log", level=logging.INFO)
+logging.basicConfig(filename="bot.log", level=logging.INFO, format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 PROXY = {'proxy_url': settings.PROXY_URL,
     'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
@@ -11,7 +13,16 @@ PROXY = {'proxy_url': settings.PROXY_URL,
 def greet_user(update, context):
     print("Вызван /start")
     print(update)
-    update.message.reply_text("Привет, пользователь!")    
+    update.message.reply_text("Привет, пользователь!")
+
+def planet(update, context):
+    planet_name = update.message.text.split()[1].lower().capitalize()
+    try:
+        planet = getattr(ephem, planet_name)()
+        planet.compute()
+        update.message.reply_text(ephem.constellation(planet))
+    except AttributeError:
+        update.message.reply_text('Введите корректное название планеты на латинице')
 
 def talk_to_me(update, context):
     text = update.message.text
@@ -23,6 +34,7 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))    
 
     logging.info("Бот стартовал")
